4d564814e305d4cd2370886a0739115a8cab1c71
laguns23 <124314180+laguns23@users.noreply.github.com>
2023-03-04 14:33:20 -0600

    Updates to main.c
    Added 4 of my recommendations, 1 of my rules, and made a few updates to the rest of the code.
    Not sure if the cost function is working as intended (though only tried it once).
    logUser function added (for my rule), asks for a code at the start of the application and saves it to a new file (doesn't do much other than that).
    erase function added and used in logUser, mainly just to delete the code the user enters to ensure its security.
    Adding the rest of my rules would take a good bit of work to incorporate into this (two deal with RNG), but we don't need *all* of our rules and recommendations in a single example, so I just aimed for 5 out of the 9 things I covered.

--- main.c
+++ main.c
@@ -23,6 +23,9 @@ void show(student *p, int *num);
 void open(student *p, int *num, int flag);
 void save(student *p, int *num);
 void cost(student *p, int *num);
+void logUser();
+void *erase(void *pointer);
+
 
 student *data = NULL;
 
@@ -31,6 +34,8 @@ int main()
     //DCL30-C: Declares following variables within execution of program but not outside of it
     int num = 0;
     int flag = 0;
+
+    logUser();
     menu(flag++, &num);
     return 0;
 }
@@ -39,42 +44,55 @@ void menu(int flag, int *num)
 {
     int choice = 0;
 
-    printf("Welcome to the class management system!\n");
-    printf("\t1) Add Students\n\t2) View Class\n\t3) Save Class as File\n\t4) Load Class File\n\t5) Cost of Class\n\t6) Quit\n");
-
-    scanf("%d", &choice);
-
-    switch (choice)
+    printf("\nWelcome to the class management system!\n");
+    do
     {
-    case 1:
-        printf("Enter the number of students in the class: ");
-        scanf("%d", num);
+        printf("\t1) Add Students\n\t2) View Class\n\t3) Save Class as File\n\t4) Load Class File\n\t5) Cost of Class\n\t6) Quit\nEnter Option: ");
+
+        scanf("%d", &choice);
+
+        // MSC20-C: Switch statement does not transition into complex blocks, instead performs simplistic instructions for each case.
+        // MSC01-C: Logical completeness in break statements for cases as appropriate, also seen in if statements with matching else statements.
+        // MSC17-C: Each switch case has a break statement with the (acceptable) exception of a clearly acknowledged instance of fallthrough.
+        switch (choice)
+        {
+            case 1:
+                printf("Enter the number of students in the class: ");
+                scanf("%d", num);
+                printf("\n");
+                init(num);
+                read(data, num);
+                break;
+            case 2:
+                show(data, num);
+                // ***Fallthrough intended here.
+            case 3:  
+                save(data, num);
+                break;
+            case 4:
+                open(data, num, flag);
+                break;
+            case 5:
+                cost(data, num);
+                break;
+            case 6:
+                printf("Quitting application...");
+                break;
+            default:
+                printf("\033[0;31mError: Cannot identify your input!\033[0m");
+                break;
+        }
         printf("\n");
-        init(num);
-        read(data, num);
-        return;
-    case 2:
-        show(data, num);
-    case 3:
-        save(data, num);
-        return;
-    case 4:
-        open(data, num, flag);
-    case 5:
-        cost(data, num);
-        return;
-    case 6:
-        exit(0);
-    default:
-        printf("\033[0;31mError: Cannot identify your input!\033[0m");
-        return;
-    }
+    } while (choice != 6);
+    exit(0);
 }
 
 void init(int *num)
 {
     //MEM35-C: Sufficient memory is allocated for data based on size of student
     data = (student *)malloc((*num) * sizeof(student));
+    // MSC15-C: In loops such as this one, rather than checking if the current element has gone beyond its bounds (which could easily
+    // lead to undefined behavior), the element is checked to ensure it remains within its bounds, ending the loop upon reaching said bounds.
     for (int i = 0; i < *num; i++)
     {
         memset(data->name, 0, NAME_LENGTH * sizeof(char));
@@ -92,7 +110,7 @@ void read(student *p, int *num)
     {
         printf("\tStudent Name: ");
         scanf("%s", p->name);
-        printf("\tStudent Gender [1=Male, 2=Female, _=Other]: ");
+        printf("\tStudent Gender [1=Male, 2=Female, 3=Other]: ");
         scanf("%d", &(p->gender));
         printf("\tStudent Age: ");
         scanf("%d", &(p->age));
@@ -128,6 +146,7 @@ void save(student *p, int *num)
         fwrite(p, sizeof(student), *num, fp);
         fclose(fp);
     }
+    printf("Class Saved.\n");
     menu(1, num);
 }
 
@@ -162,4 +181,33 @@ void cost(student *p, int *num) {
     printf("Total cost will be: %d", CALCULATE_COST(*costPer, *num));
     free(costPer); //MEM30-C: Since costPer is being freed here, it will not be accessed anymore
     menu(1, num);
+}
+
+// MSC41-C: This code to log the last user is erased immediately after the file it is written to is closed, ensuring its security
+// (at least within the application).
+void logUser()
+{
+    char code[20];
+
+    printf("Enter a unique code that is 20 characters or less to log who last used the system (only admins will have access to that file): ");
+    fgets(code, sizeof(code), stdin);
+
+    FILE *fp = fopen("user_log.txt", "w+");
+
+    if (fp)
+    {
+        fwrite(code, sizeof(code), 1, fp);
+        fclose(fp);
+    }
+    erase(code);
+}
+
+void *erase(void *pointer)
+{
+    int sizeToRemove = sizeof(pointer);
+    volatile unsigned char *p = pointer;
+    
+    while (sizeToRemove--)
+       *p++ = 0;
+    return pointer;
 }
